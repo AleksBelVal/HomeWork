f38de5a16c9df33da6302e44ac069a71e4c6402e
Aleksandr <belyakov71@yandex.ru>
2022-08-10 23:14:22 +0300

    Домашняя работа. Задача №4

---
+++ HomeWork_Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача №4 nрограмма принимает вход 3 числа, 
+// и выдаёт максимальное число.
+Console.WriteLine("Введите первое число: ");
+var number1 = Decimal.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+var number2 = Decimal.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+var number3= Decimal.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    if (number1 > number3)
+    {
+        Console.WriteLine("Первое число - максимальное");
+    }
+    else
+    {
+        Console.WriteLine("Третье число - максимальное");
+    }
+}    
+else 
+{
+    if (number2 > number3)
+    {
+        Console.WriteLine("Второе число - максимальное");
+    }
+    else
+    {
+        Console.WriteLine("Третье число - максимальное");
+    }
+}
