bcbd52ba5f348a11a504a1a365e66fd1252954c7
Aleksandr <belyakov71@yandex.ru>
2022-08-10 22:26:21 +0300

    Домашняя работа. Задача №2

---
+++ HomeWork_Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №2 nрограмма принимает вход 2 числа, 
+// и выдаёт какое число больше, а какое меньше.
+Console.WriteLine("Введите первое число: ");
+var number1 = Decimal.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+var number2 = Decimal.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("Первое число, больше второго");
+}
+else 
+{
+    Console.WriteLine("Второе число, больше первого");
+}
