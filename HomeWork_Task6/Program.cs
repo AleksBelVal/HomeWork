05b8c9141b9976b64db84ca80d8548fba67bdfe5
Aleksandr <belyakov71@yandex.ru>
2022-08-11 00:14:51 +0300

    Домашняя работа. Задача №6

---
+++ HomeWork_Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №6 определить четность введённого числа (делится ли оно на 2 без остатка)
+Console.WriteLine("Введите целое число: ");
+int a = Convert.ToInt16(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число - чётное");
+}
+else 
+{
+    Console.WriteLine("Число нечётное");
+}
